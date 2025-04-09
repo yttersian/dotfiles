30238e9369c48ce33e34e1999b848e4cba1eb66c
Torbjørn Yttersian <torbjorn.yttersian@appear.net>
2025-04-09 15:21:07 +0200

    Add brew support on Linux

--- fish/.config/fish/conf.d/00_initialize.fish
+++ fish/.config/fish/conf.d/00_initialize.fish
@@ -2,6 +2,10 @@ function fish_greeting
     # echo twice is the best gg
 end
 
+if test -x /home/linuxbrew/.linuxbrew/bin/brew
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+end
+
 if not contains "$HOME/.local/bin" $PATH
     set -x PATH "$HOME/.local/bin" $PATH
 end
