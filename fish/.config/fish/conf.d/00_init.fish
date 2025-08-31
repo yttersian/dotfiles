cb6298837ac9c22c1b596320c6be18000afb4737
Torbjørn Yttersian <torbjorn.yttersian@gmail.com>
2025-08-31 15:34:35 +0200

    Update fish configs

--- fish/.config/fish/conf.d/00_init.fish
+++ fish/.config/fish/conf.d/00_init.fish
@@ -6,6 +6,6 @@ if test -x /home/linuxbrew/.linuxbrew/bin/brew
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 end
 
-if not contains "$HOME/.local/bin" $PATH
-    set -x PATH "$HOME/.local/bin" $PATH
-end
+fish_add_path --global --move "$HOME/.local/bin"
+
+set -x XCOMPOSEFILE ~/.XCompose
